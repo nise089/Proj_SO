41f102657a4f68bdba15ea8f88b7cfaa8f081e0b
Denise <denise.huber@ur.de>
2022-10-05 13:20:28 +0200

    add slider task from https://github.com/qwiglydee/otree-experiments and adjust requirements accordingly

--- settings.py
+++ settings.py
@@ -1,17 +1,31 @@
 from os import environ
-SESSION_CONFIG_DEFAULTS = dict(real_world_currency_per_point=1, participation_fee=0)
+
 SESSION_CONFIGS = [
     dict(name='Test_Klima',
         num_demo_participants=4,
         app_sequence=['app_Welcome']
-         )
+         ),
+    dict(
+        name="sliders",
+        display_name="RET Slider task",
+        num_demo_participants=1,
+        app_sequence=["app_sliders"],
+    )
 ]
+
+# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
+# in SESSION_CONFIGS, except those that explicitly override it.
+# the session config can be accessed from methods in your apps as self.session.config,
+# e.g. self.session.config['participation_fee']
+
+SESSION_CONFIG_DEFAULTS = dict(real_world_currency_per_point=1, participation_fee=0)
+
 LANGUAGE_CODE = 'de'
 REAL_WORLD_CURRENCY_CODE = 'EUR'
 USE_POINTS = True
 DEMO_PAGE_INTRO_HTML = ''
-PARTICIPANT_FIELDS = []
-SESSION_FIELDS = []
+PARTICIPANT_FIELDS = ['is_dropout']
+SESSION_FIELDS = ['params']
 ROOMS = []
 
 ADMIN_USERNAME = 'admin'
